38a62671ec526e8eb61b5d51c1f9608dde7f2da8
Juan Gustavo Perez <74439821+juangperezdev@users.noreply.github.com>
2024-02-24 08:48:21 -0300

    change name restaurant

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
         <div className="flex items-center justify-between w-full p20" >
           <div className="flex items-center">
           <img src={logo} alt="Logo" className="h-8 w-18 mr-2" />
-          <span className="text-white text-lg pr-12 font-semibold">Menú Online</span>
+          <span className="text-white text-lg pr-12 font-semibold">Restaurant Pepito</span>
           </div>
         </div>
        
